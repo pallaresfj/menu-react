06526818057c91945a944e12bbb9828c9dbd3e24
kadolakatt <klacatt@uninorte.edu.co>
2021-11-24 23:05:09 -0500

    Cambios href MenuBar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -11,11 +11,11 @@ const MenuBar = function({ onOptionClicked }) {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Menu" align="end" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1" name="comp1" onClick={onOptionClicked}>Componente 1</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2" name="comp2" onClick={onOptionClicked}>Componente 2</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3" name="comp3" onClick={onOptionClicked}>Componente 3</NavDropdown.Item>
+                            <NavDropdown.Item href="#Componente1" name="comp1" onClick={onOptionClicked}>Componente 1</NavDropdown.Item>
+                            <NavDropdown.Item href="#Componente2" name="comp2" onClick={onOptionClicked}>Componente 2</NavDropdown.Item>
+                            <NavDropdown.Item href="#Componente3" name="comp3" onClick={onOptionClicked}>Componente 3</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4" name="comp4" onClick={onOptionClicked}>Componente 4</NavDropdown.Item>
+                            <NavDropdown.Item href="#Componente4" name="comp4" onClick={onOptionClicked}>Componente 4</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>                    
                 </Navbar.Collapse>
